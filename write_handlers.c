f8d3971777c1cea513b6df4cf88dd7ebe34c4dd5
Conquernm <nghwazinm@gmail.com>
2023-07-24 23:11:21 +0200

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -3,7 +3,7 @@
 
 /************************* WRITE HANDLE *************************/
 /**
- * handle_write_char - Prints a string
+ * handle_write_char - Prints a str
  * @c: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
