1e98c10081305f1128a8859c498fe7d89eb38eb1
Conquernm <nghwazinm@gmail.com>
2023-07-24 21:53:12 +0200

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
+ * get_width - Calculates the width for print
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
