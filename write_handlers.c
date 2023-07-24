f5f07f105d5b0b788fe1f9d493b0594b7bfb3022
Conquernm <nghwazinm@gmail.com>
2023-07-24 23:56:53 +0200

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -47,7 +47,7 @@ int handle_write_char(char c, char buffer[],
 /************************* WRITE NUMBER *************************/
 /**
  * write_number - Prints a string
- * @is_negative: List of arguments
+ * @is_negative: List of arguments.
  * @ind: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
