0042d7cf7b30dc0e36da8fc268f555b6fc80b57e
Conquernm <nghwazinm@gmail.com>
2023-07-25 00:29:41 +0200

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -2,13 +2,13 @@
 
 /************************* WRITE HANDLE *************************/
 /**
- * handle_write_char - Prints a string
- * @c: char types
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: precision specifier
- * @size: Size specifier
+ * handle_write_char - Prints a string.
+ * @c: char types.
+ * @buffer: Buffer array to handle print.
+ * @flags:  Calculates active flags.
+ * @width: get width.
+ * @precision: precision specifier.
+ * @size: Size specifier.
  *
  * Return: Number of chars printed.
  */
@@ -46,14 +46,14 @@ int handle_write_char(char c, char buffer[],
 
 /************************* WRITE NUMBER *************************/
 /**
- * write_number - Prints a string
- * @is_negative: List of arguments
- * @ind: char types
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: precision specifier
- * @size: Size specifier
+ * write_number - Prints a string.
+ * @is_negative: List of arguments.
+ * @ind: char types.
+ * @buffer: Buffer array to handle print.
+ * @flags:  Calculates active flags.
+ * @width: get width.
+ * @precision: precision specifier.
+ * @size: Size specifier.
  *
  * Return: Number of chars printed.
  */
